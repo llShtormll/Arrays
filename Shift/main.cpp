016c0cc1e835804f0e0f90aacee79415aaf8c2f5
llShtormll <149567370+llShtormll@users.noreply.github.com>
2025-03-19 07:58:20 +0300

    Update main.cpp

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -9,16 +9,12 @@ void main()
 
 	int arr[10] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
 	int n, k = 0;
-	cout << "Ââåäèòå ñìåùåíèå ìàñèâà :"; cin >> n;
+	cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã±Ã¬Ã¥Ã¹Ã¥Ã­Ã¨Ã¥ Ã¬Ã Ã±Ã¨Ã¢Ã  :"; cin >> n;
 	cout << endl;
 	for (int i = 0 + n; i < 10 + n; i++)
 	{
 		if (i < 10)cout << arr[i] << "\t";
-		else if (i > 9)
-		{
-			k = i - 10;
-			cout << arr[k] << "\t";
-		}
+		else if (i > 9){k = i - 10;cout << arr[k] << "\t";}
 	}
 
 
